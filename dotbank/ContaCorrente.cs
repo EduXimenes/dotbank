516f1d64617bb3d53779cfa9b9acbe89ca01524e
EduXimenes <e.ximenes17@hotmail.com>
2022-06-27 17:11:57 -0300

    First commit

---
+++ dotbank/ContaCorrente.cs
@@ -0,0 +1,50 @@
+﻿namespace dotbank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else { 
+            saldo = saldo + valor;
+            }
+        }
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+    }
+}
---
+++ dotbank/Program.cs
@@ -0,0 +1,30 @@
+﻿using dotbank;
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Eduardo Ximenes";
+conta1.conta = "12345-x";
+conta1.numero_agencia = 17;
+conta1.nome_agencia = "Santander";
+conta1.saldo = 1000;
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Rodolfo";
+conta2.conta = "3235-x";
+conta2.numero_agencia = 17;
+conta2.nome_agencia = "Santander";
+conta2.saldo = 100;
+
+
+Console.WriteLine("Bem vindo ao DotBank!!");
+
+Console.WriteLine("Conta do Eduardo saldo atual: " + conta1.saldo);
+Console.WriteLine("Conta do Rodolfo saldo atual: " + conta2.saldo);
+
+Titular titular = new Titular();
+titular.nome = "Eduardo";
+titular.cpf = "12345678";
+titular.profissao = "carry";
+
+
+
+Console.ReadKey();
